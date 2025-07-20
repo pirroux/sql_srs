175d6498aefd7d055ac2b2b4ccc1e31e9a0be44c
pierre canadas <pierre.canadas@gmail.com>
2025-07-20 18:00:10 +0200

    add ci and cleanup code with black and pylint

--- app.py
+++ app.py
@@ -1,24 +1,25 @@
-import streamlit as st
-import pandas as pd
-import duckdb
+# pylint: disable = missing-module-docstring
 import io
 
+import duckdb
+import pandas as pd
+import streamlit as st
 
-csv ='''
+csv = """
 beverage, price
 coffee, 1.50
 tea, 1.00
 water, 0.75
-'''
+"""
 
 beverages = pd.read_csv(io.StringIO(csv))
 
-csv2 = '''
+csv2 = """
 food_item, price
 cookie, 1.00
 chocolatine, 1.50
 muffin, 2.00
-'''
+"""
 food_items = pd.read_csv(io.StringIO(csv2))
 
 answer = """
@@ -27,13 +28,17 @@ CROSS JOIN food_items
 """
 
 
+def my_func():
+    print("hello")
 
 
 solution = duckdb.sql(answer).df()
 
 with st.sidebar:
     st.title("what do you want to work on?")
-    table = st.selectbox(label="tables", key="tables", options=["beverages", "food_items"])
+    table = st.selectbox(
+        label="tables", key="tables", options=["beverages", "food_items"]
+    )
     if table == "beverages":
         st.write(beverages)
     elif table == "food_items":
@@ -43,12 +48,32 @@ with st.sidebar:
 
 st.header("enter your query")
 query = st.text_area(label="votre_code_sql", key="query", height=200)
+
 if query:
+    results = duckdb.sql(query).df()
+    st.dataframe(results)
+
     try:
-        result = duckdb.sql(query).df()
-        st.write(result)
-    except Exception as e:
-        st.error(f"Erreur: {e}")
+        results = results[solution.columns]
+        st.dataframe(results.compare(solution))
+    except KeyError as e:
+        st.write("columns not in the same ORDER or are not the same as solution")
+        # st.write("somethihng is odd")
+
+    # if len(results.columns) != len(solution.columns):
+    #     st.write("nombre de colomnes différentes")
+
+    # n_lines_diff = results.shape[0] - solution.shape[0]
+    # if n_lines_diff != 0:
+    #     st.write(" not the same lenght nigga")
+
+
+# if query:
+#     try:
+#         result = duckdb.sql(query).df()
+#         st.write(result)
+#     except Exception as e:
+#         st.error(f"Erreur: {e}")
 
 tab2, tab3 = st.tabs(["tables", "solution"])
 
