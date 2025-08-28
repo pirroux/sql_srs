ee77ac24b768a8cb2293c51c17100adff77570be
pierre canadas <pierre.canadas@gmail.com>
2025-08-28 16:30:51 +0200

    add ci and cleanup code with Black and Pylint

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-import streamlit as st
-import pandas as pd
-import duckdb
+# pylint: disable = missing-module-docstring
+
 import io
 
+import duckdb
+import pandas as pd
+import streamlit as st
 import duckdb
 import pandas as pd
 import streamlit as st
@@ -14,15 +16,16 @@ tea, 1.00
 water, 0.75
 """
 
+
 beverages = pd.read_csv(io.StringIO(CSV))
 
-csv2 = '''
+CSV2 = """
 food_item, price
 cookie, 1.00
 chocolatine, 1.50
 muffin, 2.00
-'''
-food_items = pd.read_csv(io.StringIO(csv2))
+"""
+food_items = pd.read_csv(io.StringIO(CSV2))
 
 ANSWER = """
 SELECT * FROM beverages
@@ -41,6 +44,9 @@ with st.sidebar:
     table = st.selectbox(
         label="tables", key="tables", options=["beverages", "food_items"]
     )
+    table = st.selectbox(
+        label="tables", key="tables", options=["beverages", "food_items"]
+    )
     if table == "beverages":
         st.write(beverages)
     elif table == "food_items":
@@ -51,11 +57,35 @@ with st.sidebar:
 st.header("enter your query")
 query = st.text_area(label="votre_code_sql", key="query", height=200)
 
+
 if query:
     results = duckdb.sql(query).df()
     st.dataframe(results)
 
+    results = duckdb.sql(query).df()
+    st.dataframe(results)
+
     try:
+        results = results[solution.columns]
+        st.dataframe(results.compare(solution))
+    except KeyError as e:
+        st.write("columns not in the same ORDER or are not the same as solution")
+        # st.write("somethihng is odd")
+
+    # if len(results.columns) != len(solution.columns):
+    #     st.write("nombre de colomnes différentes")
+
+    # n_lines_diff = results.shape[0] - solution.shape[0]
+    # if n_lines_diff != 0:
+    #     st.write(" not the same lenght nigga")
+
+
+# if query:
+#     try:
+#         result = duckdb.sql(query).df()
+#         st.write(result)
+#     except Exception as e:
+#         st.error(f"Erreur: {e}")
         results = results[solution.columns]
         st.dataframe(results.compare(solution))
     except KeyError as e:
