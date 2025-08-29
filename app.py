6d7003ba80bffd67b7b77737b21197e38b4c1eeb
pirroux <155838356+pirroux@users.noreply.github.com>
2025-08-29 17:07:22 +0200

    ajout d'une database avec les exercises et memorystate (#23)
    * ajout d'une database avec les exercises et memorystate
    
    * fix: apply black, isort and pylint corrections

--- app.py
+++ app.py
@@ -1,117 +1,91 @@
 # pylint: disable = missing-module-docstring
 
-import io
+import ast
 
 import duckdb
-import pandas as pd
 import streamlit as st
 
-CSV = """
-beverage, price
-coffee, 1.50
-tea, 1.00
-water, 0.75
-"""
+con = duckdb.connect(database="data/exercises_sql_tables.duckdb", read_only=False)
 
-beverages = pd.read_csv(io.StringIO(CSV))
-
-CSV2 = """
-food_item, price
-cookie, 1.00
-chocolatine, 1.50
-muffin, 2.00
-"""
-food_items = pd.read_csv(io.StringIO(CSV2))
-
-ANSWER = """
-SELECT * FROM beverages
-CROSS JOIN food_items
-"""
-
-
-def my_func():
-    print("hello")
-
-
-solution = duckdb.sql(ANSWER).df()
+# solution = duckdb.sql(ANSWER).df()
 
 with st.sidebar:
-    st.title("what do you want to work on?")
-    table = st.selectbox(
-        label="tables", key="tables", options=["beverages", "food_items"]
+    theme = st.selectbox(
+        "what would you like to work on",
+        ("cross_joins", "group_by", "window_functions"),
+        placeholder="Select a theme...",
     )
-    table = st.selectbox(
-        label="tables", key="tables", options=["beverages", "food_items"]
-    )
-    if table == "beverages":
-        st.write(beverages)
-    elif table == "food_items":
-        st.write(food_items)
-    else:
-        st.write("no table selected")
+    st.write("You chose:", theme)
+
+    exercise = con.execute(f"SELECT * FROM memory_state WHERE theme = '{theme}' ").df()
+    st.write(exercise)
 
 st.header("enter your query")
 query = st.text_area(label="votre_code_sql", key="query", height=200)
 
-
 if query:
-    results = duckdb.sql(query).df()
+    results = con.execute(query).df()
     st.dataframe(results)
+#     results = duckdb.sql(query).df()
+#     st.dataframe(results)
 
-    results = duckdb.sql(query).df()
-    st.dataframe(results)
+#     results = duckdb.sql(query).df()
+#     st.dataframe(results)
 
-    try:
-        results = results[solution.columns]
-        st.dataframe(results.compare(solution))
-    except KeyError as e:
-        st.write("columns not in the same ORDER or are not the same as solution")
-        # st.write("somethihng is odd")
-
-        # if len(results.columns) != len(solution.columns):
-        #     st.write("nombre de colomnes différentes")
-
-        # n_lines_diff = results.shape[0] - solution.shape[0]
-        # if n_lines_diff != 0:
-        #     st.write(" not the same lenght nigga")
-
-        # if query:
-        #     try:
-        #         result = duckdb.sql(query).df()
-        #         st.write(result)
-        #     except Exception as e:
-        #         st.error(f"Erreur: {e}")
-        results = results[solution.columns]
-        st.dataframe(results.compare(solution))
-    except KeyError as e:
-        st.write("columns not in the same ORDER or are not the same as solution")
-        # st.write("somethihng is odd")
-
-    # if len(results.columns) != len(solution.columns):
-    #     st.write("nombre de colomnes différentes")
-
-    # n_lines_diff = results.shape[0] - solution.shape[0]
-    # if n_lines_diff != 0:
-    #     st.write(" not the same lenght nigga")
-
-
-# if query:
 #     try:
-#         result = duckdb.sql(query).df()
-#         st.write(result)
-#     except Exception as e:
-#         st.error(f"Erreur: {e}")
+#         results = results[solution.columns]
+#         st.dataframe(results.compare(solution))
+#     except KeyError as e:
+#         st.write("columns not in the same ORDER or are not the same as solution")
+#         # st.write("somethihng is odd")
+
+#         # if len(results.columns) != len(solution.columns):
+#         #     st.write("nombre de colomnes différentes")
+
+#         # n_lines_diff = results.shape[0] - solution.shape[0]
+#         # if n_lines_diff != 0:
+#         #     st.write(" not the same lenght nigga")
+
+#         # if query:
+#         #     try:
+#         #         result = duckdb.sql(query).df()
+#         #         st.write(result)
+#         #     except Exception as e:
+#         #         st.error(f"Erreur: {e}")
+#         results = results[solution.columns]
+#         st.dataframe(results.compare(solution))
+#     except KeyError as e:
+#         st.write("columns not in the same ORDER or are not the same as solution")
+#         # st.write("somethihng is odd")
+
+#     # if len(results.columns) != len(solution.columns):
+#     #     st.write("nombre de colomnes différentes")
+
+#     # n_lines_diff = results.shape[0] - solution.shape[0]
+#     # if n_lines_diff != 0:
+#     #     st.write(" not the same lenght nigga")
+
+
+# # if query:
+# #     try:
+# #         result = duckdb.sql(query).df()
+# #         st.write(result)
+# #     except Exception as e:
+# #         st.error(f"Erreur: {e}")
 
 tab2, tab3 = st.tabs(["tables", "solution"])
 
 with tab2:
-    st.write("table: beverages")
-    st.dataframe(beverages)
-    st.write("table: food_items")
-    st.dataframe(food_items)
-    st.write("expected result")
-    st.dataframe(solution)
+    exercise_tables = ast.literal_eval(exercise.loc[0, "tables"])
+    for table in exercise_tables:
+        st.write(f"table: {table}")
+        df_table = con.execute(f"SELECT * FROM {table}")
+        st.dataframe(df_table)
+#     st.write("table: food_items")
+#     st.dataframe(food_items)
+#     st.write("expected result")
+#     st.dataframe(solution)
 
 
-with tab3:
-    st.write(ANSWER)
+# with tab3:
+#     st.write(ANSWER)
