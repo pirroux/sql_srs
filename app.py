5e08af41e6265f6bc7f88399d80a67c3cc1c9250
pirroux <155838356+pirroux@users.noreply.github.com>
2025-08-30 13:25:14 +0200

    addition of the Answer (#26)
    addition of solution and comparison with user input

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 # pylint: disable = missing-module-docstring
 
 import ast
-import io
 
 import duckdb
 import streamlit as st
@@ -21,72 +20,44 @@ with st.sidebar:
     exercise = con.execute(f"SELECT * FROM memory_state WHERE theme = '{theme}' ").df()
     st.write(exercise)
 
+    # Getting the exercise solution
+    exercise_name = exercise.loc[0, "exercise_name"]
+    with open(f"answers/{exercise_name}.sql", "r", encoding="utf-8") as f:
+        answer = f.read()
+    solution_df = con.execute(answer).df()
+
 st.header("enter your query")
 query = st.text_area(label="votre_code_sql", key="query", height=200)
 
 if query:
     results = con.execute(query).df()
     st.dataframe(results)
-#     results = duckdb.sql(query).df()
-#     st.dataframe(results)
 
-#     results = duckdb.sql(query).df()
-#     st.dataframe(results)
+    # verifying the columns
+    try:
+        results = results[solution_df.columns]
+        st.dataframe(results.compare(solution_df))
+    except KeyError as e:
+        st.write("Some columns are missing")
 
-#     try:
-#         results = results[solution.columns]
-#         st.dataframe(results.compare(solution))
-#     except KeyError as e:
-#         st.write("columns not in the same ORDER or are not the same as solution")
-#         # st.write("somethihng is odd")
+    # verifying the amount of lines
+    n_lines_difference = results.shape[0] - solution_df.shape[0]
+    if n_lines_difference != 0:
+        st.write(
+            f"results has a {n_lines_difference} lines difference with the solution"
+        )
 
-#         # if len(results.columns) != len(solution.columns):
-#         #     st.write("nombre de colomnes différentes")
-
-#         # n_lines_diff = results.shape[0] - solution.shape[0]
-#         # if n_lines_diff != 0:
-#         #     st.write(" not the same lenght nigga")
-
-#         # if query:
-#         #     try:
-#         #         result = duckdb.sql(query).df()
-#         #         st.write(result)
-#         #     except Exception as e:
-#         #         st.error(f"Erreur: {e}")
-#         results = results[solution.columns]
-#         st.dataframe(results.compare(solution))
-#     except KeyError as e:
-#         st.write("columns not in the same ORDER or are not the same as solution")
-#         # st.write("somethihng is odd")
-
-#     # if len(results.columns) != len(solution.columns):
-#     #     st.write("nombre de colomnes différentes")
-
-#     # n_lines_diff = results.shape[0] - solution.shape[0]
-#     # if n_lines_diff != 0:
-#     #     st.write(" not the same lenght nigga")
-
-
-# # if query:
-# #     try:
-# #         result = duckdb.sql(query).df()
-# #         st.write(result)
-# #     except Exception as e:
-# #         st.error(f"Erreur: {e}")
-
-tab2, tab3 = st.tabs(["tables", "solution"])
+tab2, tab3 = st.tabs(["Tables", "Solution"])
 
 with tab2:
     exercise_tables = ast.literal_eval(exercise.loc[0, "tables"])
     for table in exercise_tables:
         st.write(f"table: {table}")
-        df_table = con.execute(f"SELECT * FROM {table}")
+        df_table = con.execute(
+            f"SELECT * FROM memory_state WHERE theme = '{theme}' "
+        ).df()
         st.dataframe(df_table)
-#     st.write("table: food_items")
-#     st.dataframe(food_items)
-#     st.write("expected result")
-#     st.dataframe(solution)
 
-
-# with tab3:
-#     st.write(ANSWER)
+with tab3:
+    st.write(answer)
+    
